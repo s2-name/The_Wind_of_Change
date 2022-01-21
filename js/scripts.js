2669ca6e8bf619585cc6297f60c3092353fa7a53
S2 <80480605+s2-name@users.noreply.github.com>
2022-01-21 23:26:00 +0300

    feedbacks

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+	// -------------------nav menu--------------------------
 	let nav_toggle_button = document.querySelector('.toggle_button');
 	if (nav_toggle_button) {
 		nav_toggle_button.addEventListener('click', function(){
@@ -8,6 +9,8 @@ document.addEventListener("DOMContentLoaded", function(){
 		});
 	}
 
+
+// ------------------------header backgrounds-----------------
 	let header = document.querySelector('header');
 	if (header) {
 		let set = header.getAttribute('data-bgset').split(', ');
@@ -22,5 +25,24 @@ document.addEventListener("DOMContentLoaded", function(){
 			}
 			header.style.backgroundImage = 'linear-gradient(45deg, #484848a3, #000000eb), url("img/header-backgrounds/'+set[i]+'")';
 		}, 5000);
+	}
+
+
+// ----------------------feedbacks----------------------------
+	let feedback_block = document.querySelector('.feedback_sity');
+	if (feedback_block) {
+		let feedbacks = JSON.parse(feedbackJSON);
+		let author_block = feedback_block.querySelector('.feedback_author');
+		let name = author_block.querySelector('span');
+		let img = author_block.querySelector('img');
+		let feedback = feedback_block.querySelector('.feedback');
+		let randomFB = parseInt(Math.random() * (feedbacks.length - 0) + 0);
+		name.innerHTML = feedbacks[randomFB]['name'];
+		feedback.innerHTML = feedbacks[randomFB]['feedback'];
+		if (feedbacks[randomFB]['avatar']) {
+			img.src = 'img/users-avatars/'+feedbacks[randomFB]['avatar'];
+		}else {
+			img.src = 'img/users-avatars/user-default-avatar.png';
+		}
 	}
 });
